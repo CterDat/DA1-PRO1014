1ac861c65434ab5cf8539806e470cdd8d1cd8c20
huyngo0211 <ngovandat13032004@gmail.com>
2023-11-19 02:14:32 +0700

    da1

--- model/pdo.php
+++ model/pdo.php
@@ -3,7 +3,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=PRO1014-DA1-DTCshop;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=pro1014-da1-dtcshop;charset=utf8";
     $username = 'root';
     $password = '';
 
