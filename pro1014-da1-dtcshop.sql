dc9b4cf2c83ae2bf439780d7b6e8a7383726bea3
huyngo0211 <ngovandat13032004@gmail.com>
2023-11-23 18:14:30 +0700

    Update pro1014-da1-dtcshop.sql

--- pro1014-da1-dtcshop.sql
+++ pro1014-da1-dtcshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2023 at 12:33 PM
+-- Generation Time: Nov 22, 2023 at 04:28 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,6 +35,34 @@ CREATE TABLE `banner` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `bill_name` varchar(255) NOT NULL,
+  `bill_adress` varchar(255) NOT NULL,
+  `bill_tel` varchar(50) NOT NULL,
+  `bill_email` int(100) NOT NULL,
+  `bill_pttt` tinyint(4) NOT NULL DEFAULT 1 COMMENT '1. Thanh toán tại nhà 2. Chuyển khoản 3. Thanh toán online',
+  `total` int(11) NOT NULL DEFAULT 0,
+  `bill_status` tinyint(4) DEFAULT 0 COMMENT '0. Đang chờ 1. Đang xử lí 2. Đang giao hàng 3. Đã giao hàng',
+  `receive_name` varchar(255) DEFAULT NULL,
+  `receive_address` varchar(255) DEFAULT NULL,
+  `receive_tel` varchar(50) DEFAULT NULL,
+  `ngaydathang` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `bill_name`, `bill_adress`, `bill_tel`, `bill_email`, `bill_pttt`, `total`, `bill_status`, `receive_name`, `receive_address`, `receive_tel`, `ngaydathang`) VALUES
+(1, 'admin', 'datnvph34326@fpt.edu.vn', '0357717435', 0, 0, 13, 0, NULL, NULL, NULL, 'h:i:sa d/m/Y');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `binhluan`
 --
 
@@ -49,6 +77,31 @@ CREATE TABLE `binhluan` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idpro` int(11) NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` int(10) NOT NULL DEFAULT 0,
+  `soluong` int(3) NOT NULL,
+  `thanhtien` int(10) NOT NULL,
+  `idbill` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `iduser`, `idpro`, `img`, `name`, `price`, `soluong`, `thanhtien`, `idbill`) VALUES
+(1, 1, 4, 'DALL·E 2023-11-07 19.54.51 - Design a captivating logo for a late-night food delivery service that creatively merges the letters D, T, and C. The composition should be centered ar.png', 'Thịt nướng 3 vị', 13, 1, 13, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `danhmuc`
 --
 
@@ -56,6 +109,14 @@ CREATE TABLE `danhmuc` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`id`, `name`) VALUES
+(1, 'Thịt nướng'),
+(2, 'Yakurt');
 
 -- --------------------------------------------------------
 
@@ -85,6 +146,16 @@ CREATE TABLE `sanpham` (
   `iddm` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `sanpham`
+--
+
+INSERT INTO `sanpham` (`id`, `name`, `price`, `img`, `mota`, `luotxem`, `iddm`) VALUES
+(1, 'Thịt nướng 3 vị', 13.00, 'DALL·E 2023-11-07 19.54.51 - Design a captivating logo for a late-night food delivery service that creatively merges the letters D, T, and C. The composition should be centered ar.png', '3 vị cay giòn', 0, 1),
+(2, 'Thịt nướng 3 vị', 13.00, 'DALL·E 2023-11-07 19.54.51 - Design a captivating logo for a late-night food delivery service that creatively merges the letters D, T, and C. The composition should be centered ar.png', '3 vị cay giòn', 0, 1),
+(3, 'Thịt nướng 3 vị', 13.00, 'DALL·E 2023-11-07 19.54.51 - Design a captivating logo for a late-night food delivery service that creatively merges the letters D, T, and C. The composition should be centered ar.png', '3 vị cay giòn', 0, 1),
+(4, 'Thịt nướng 3 vị', 13.00, 'DALL·E 2023-11-07 19.54.51 - Design a captivating logo for a late-night food delivery service that creatively merges the letters D, T, and C. The composition should be centered ar.png', '3 vị cay giòn', 0, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -102,8 +173,21 @@ CREATE TABLE `taikhoan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`id`, `user`, `pass`, `email`, `address`, `tel`, `role`) VALUES
+(1, 'admin', '123456', 'datnvph34326@fpt.edu.vn', 'Tân Lập', '0357717435', 1);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `binhluan`
@@ -112,6 +196,12 @@ ALTER TABLE `binhluan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_bl_sp` (`idpro`),
   ADD KEY `fk_bl_tk` (`iduser`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `danhmuc`
@@ -144,16 +234,28 @@ ALTER TABLE `taikhoan`
 --
 
 --
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `binhluan`
 --
 ALTER TABLE `binhluan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `dsbanner`
@@ -165,13 +267,13 @@ ALTER TABLE `dsbanner`
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `taikhoan`
 --
 ALTER TABLE `taikhoan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
